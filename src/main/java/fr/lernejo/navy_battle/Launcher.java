4ce55ecd05dceb7b9326c016b7d17898e8adac52
Pierre Hubert <pierre.git@communiquons.org>
2021-05-19 13:52:12 +0200

    Send start game request

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -2,17 +2,25 @@ package fr.lernejo.navy_battle;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
+import fr.lernejo.navy_battle.prototypes.Option;
 import fr.lernejo.navy_battle.prototypes.ServerInfo;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.UUID;
 import java.util.concurrent.Executors;
 
 public class Launcher {
-    private ServerInfo localServer;
-    private ServerInfo remoteServer;
+    private final HttpClient client = HttpClient.newHttpClient();
+
+    private final Option<ServerInfo> localServer = new Option<>();
+    private final Option<ServerInfo> remoteServer = new Option<>();
 
     public static void main(String[] args) {
         try {
@@ -34,11 +42,14 @@ public class Launcher {
      * Start the server
      */
     private void startServer(int port, String connectURL) throws IOException {
-        localServer = new ServerInfo(
+        localServer.set(new ServerInfo(
             UUID.randomUUID().toString(),
             "http://localhost:" + port,
             "Pierre is the best coder forever. I can only beat you!"
-        );
+        ));
+
+        if (connectURL != null)
+            new Thread(() -> this.requestStart(connectURL)).start();
 
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         server.setExecutor(Executors.newSingleThreadExecutor());
@@ -64,18 +75,51 @@ public class Launcher {
      */
     public void startGame(RequestHandler handler) throws IOException {
         try {
-            if (remoteServer != null) {
+            if (remoteServer.isNotEmpty()) {
                 handler.sendString(400, "I'm sorry, I have already another player dude!");
                 return;
             }
 
-            remoteServer = ServerInfo.fromJSON(handler.getJSONObject());
+            remoteServer.set(ServerInfo.fromJSON(handler.getJSONObject()));
+            System.out.println("Will fight against " + remoteServer.get().getUrl());
 
-            handler.sendJSON(202, localServer.toJSON());
+            handler.sendJSON(202, localServer.get().toJSON());
 
         } catch (Exception e) {
             e.printStackTrace();
             handler.sendString(400, e.getMessage());
         }
+    }
+
+    /**
+     * Request the server to be started
+     */
+    public void requestStart(String server) {
+        try {
+            var response = sendHTTPRequest(server + "/api/game/start", this.localServer.get().toJSON());
+
+            this.remoteServer.set(ServerInfo.fromJSON(response));
+
+            System.out.println("Will fight against " + remoteServer.get().getUrl());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Failed to start game!");
+        }
+    }
+
+    /**
+     * Send POST request
+     */
+    public JSONObject sendHTTPRequest(String url, JSONObject obj) throws IOException, InterruptedException {
+        HttpRequest requetePost = HttpRequest.newBuilder()
+            .uri(URI.create(url))
+            .setHeader("Accept", "application/json")
+            .setHeader("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString(obj.toString()))
+            .build();
+
+        var response = client.send(requetePost, HttpResponse.BodyHandlers.ofString());
+        return new JSONObject(response.body());
     }
 }
