b3c0c6f7785790d699e15ef651c8dbc94fe7d69b
Pierre Hubert <pierre.git@communiquons.org>
2021-05-20 14:09:32 +0200

    Improve test coverage

--- src/test/java/fr/lernejo/navy_battle/prototypes/CoordinatesTest.java
+++ src/test/java/fr/lernejo/navy_battle/prototypes/CoordinatesTest.java
@@ -14,8 +14,42 @@ class CoordinatesTest {
     }
 
     @Test
+    void testFromStringInvalid() {
+        assertThrows(RuntimeException.class, () -> new Coordinates("PP"));
+        assertThrows(RuntimeException.class, () -> new Coordinates("P"));
+        assertThrows(RuntimeException.class, () -> new Coordinates("PPP"));
+    }
+
+    @Test
     void testToString() {
         var coordinates = new Coordinates(2, 6);
         assertEquals("C7", coordinates.toString());
+    }
+
+    @Test
+    void testHash() {
+        var one = new Coordinates(1, 2);
+        var two = new Coordinates(1, 2);
+        var three = new Coordinates(4, 5);
+
+        assertEquals(one, two);
+        assertNotEquals(one, three);
+
+        assertEquals(one.hashCode(), two.hashCode());
+        assertNotEquals(one.hashCode(), three.hashCode());
+    }
+
+    @Test
+    void otherTestEquals() {
+        Coordinates one = new Coordinates(1, 2);
+        assertNotEquals(one, new Object());
+
+        Object two = null;
+        if (one.getX() == 4)
+            two = new Object();
+
+        assertNotEquals(two, one);
+
+        assertEquals(one, one);
     }
 }
